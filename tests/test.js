6f9e9241212d8da38aa41947d3733914cabc2773
roccomuso <roccomusolino92@gmail.com>
2018-04-30 14:59:41 +0200

    improved tests

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,17 @@
 const test = require('tape')
 const isBing = require('../')
 
-test('should fail on wrong ip', (t) => {
-  t.plan(1)
+test('should fail on wrong IPs', (t) => {
+  t.plan(2)
   isBing('1.1.1.1').then(outcome => t.notOk(outcome))
+  isBing('123.123.123.123').catch(err => t.equal(err.code, 'ENOTFOUND'))
 })
 
 test('should fail with wrong inputs', (t) => {
-  t.plan(2)
+  t.plan(3)
   isBing('helloworld').catch(err => t.ok(err))
   isBing('0.0.0.0.0.0').catch(err => t.ok(err))
+  isBing().catch(err => t.ok(err))
 })
 
 test('should pass on valid search.msn.com crawler ip', (t) => {
